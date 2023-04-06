81ebaa8fb9a14aa2de4011b3626f440a19373df6
DEVILL-MASCOT <102029388+DEVILL-MASCOT@users.noreply.github.com>
2023-04-06 09:37:58 +0530

    Update Xiao_❤_core.js

--- Xiao_❤_core.js
+++ Xiao_❤_core.js
@@ -1,4 +1,4 @@
-require('./settings')
+require('settings')
 const { BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto, generateWAMessageContent, Mimetype, generateWAMessage, prepareWAMessageMedia, prepareMessageFromContent,  areJidsSameUser, getContentType } = require('@adiwajshing/baileys')
 const fs = require('fs')
 const os = require('os')

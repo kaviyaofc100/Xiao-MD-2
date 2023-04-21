477674ee19d6b22afeca3cb1527a1616d20cdc20
DEVILL-MASCOT <102029388+DEVILL-MASCOT@users.noreply.github.com>
2023-04-21 19:00:56 +0530

    Update xiao✌❤️‍🔥index.js

--- xiao✌❤️‍🔥index.js
+++ xiao✌❤️‍🔥index.js
@@ -1,4 +1,4 @@
-require('./lib/settings')
+require('./setting')
 const { default: NEXUSConnect, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = require("@adiwajshing/baileys")
 const { state, saveState } = useSingleFileAuthState(`${sessionName}.json`)
 const pino = require('pino')
